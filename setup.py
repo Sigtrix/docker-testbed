dba917ba3b459e563324da24b79a0194bacfb2e0
Sigtrix <hes86@hi.is>
2023-03-20 22:55:25 -0400

    ...

--- setup.py
+++ setup.py
@@ -13,15 +13,20 @@ def build_image(img_name, img_path):
 	os.system(cmd)
 
 
-def create_container(container_name, ip, img_name):
+def create_container(container_name, img_name, network, ip):
 	"""
 	Create and start container
 	:param container_name: name of container
 	:param ip: container ip address
 	:param img_name: name of image
+	:param network: network name on eth0 interface
+	:param ip: ip address of node on <network>
 	:return: None
 	"""
-	cmd = f"docker run -d --name {container_name} --ip {ip} --privileged {img_name}"
+	cmd = f"docker run -d --name {container_name}" \
+	      f" --network {network}" \
+	      f" --ip {ip}" \
+	      f" --privileged {img_name}"
 	os.system(cmd)
 
 
@@ -74,7 +79,7 @@ for link_name, link_param in links.items():
 
 # create containers
 for node_name, node_param in nodes.items():
-	create_container(node_name, node_param[0], node_param[1][0])
+	create_container(node_name, node_param[1][0], node_param[2], node_param[0])
 
 # attach containers to networks
 for link_name, link_param in links.items():
