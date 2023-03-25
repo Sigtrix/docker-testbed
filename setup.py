c401d022ef0e3db34aa7c6a83f2cd367d1dd0ffa
Sigtrix <hes86@hi.is>
2023-03-25 16:34:36 -0400

    Adds feature to configure bandwidth dynamically with tbf.

--- setup.py
+++ setup.py
@@ -4,31 +4,33 @@ import ast
 from config import nodes, links
 from queue import PriorityQueue
 
+
 def dijkstra(graph, start):
-    """Function to compute the shortest path from a source node to all other nodes in the graph"""
-    # Initialize the distance dictionary and set the distance of the start node to 0
-    dist = {}
-    for node in graph:
-        dist[node] = [float('inf'),node]
-    dist[start][0] = 0
+	"""Function to compute the shortest path from a source node to all other nodes in the graph"""
+	# Initialize the distance dictionary and set the distance of the start node to 0
+	dist = {}
+	for node in graph:
+		dist[node] = [float('inf'), node]
+	dist[start][0] = 0
 
-    # Initialize the priority queue and add the start node with distance 0
-    pq = PriorityQueue()
-    pq.put((0, start))
+	# Initialize the priority queue and add the start node with distance 0
+	pq = PriorityQueue()
+	pq.put((0, start))
 
-    # Traverse the graph using BFS and update the distance for each neighbor node
-    while not pq.empty():
-        current_dist, current_node = pq.get()
-        if current_dist > dist[current_node][0]:
-            continue
-        for neighbor, weight in graph[current_node]:
-            distance = current_dist + weight
-            if distance < dist[neighbor][0]:
-                dist[neighbor] = [distance, current_node]
-                pq.put([distance, neighbor])
+	# Traverse the graph using BFS and update the distance for each neighbor node
+	while not pq.empty():
+		current_dist, current_node = pq.get()
+		if current_dist > dist[current_node][0]:
+			continue
+		for neighbor, weight in graph[current_node]:
+			distance = current_dist + weight
+			if distance < dist[neighbor][0]:
+				dist[neighbor] = [distance, current_node]
+				pq.put([distance, neighbor])
 
-    # Return the distance dictionary
-    return dist
+	# Return the distance dictionary
+	return dist
+
 
 def build_image(img_name, img_path):
 	"""
@@ -72,11 +74,12 @@ def create_subnet(ip_range, subnet_name):
 	os.system(cmd)
 
 
-def attach(ip, subnet_name, container_name, interface):
+def attach(ip, subnet_name, container_name, interface, tc_params):
 	"""
 	Attach container to subnet
 	:param subnet_name: name of subnet
 	:param container_name: name of container
+	:param tc_params: tuple (bandwidth, burst, latency)
 	:return: None
 	"""
 	if interface != "eth0":
@@ -96,10 +99,28 @@ def add_route(container_name, ip_range, gateway_ip, interface):
 	"""
 	cmd = f"docker exec {container_name} ip route add {ip_range}" \
 	      f" via {gateway_ip} dev {interface}"
-	cmdValue = os.system(cmd)
-	if cmdValue!=0:
+	cmd_value = os.system(cmd)
+	if cmd_value != 0:
 		cmd = f"docker exec {container_name} ip route change {ip_range}" \
-	      f" via {gateway_ip} dev {interface}"
+		      f" via {gateway_ip} dev {interface}"
+		os.system(cmd)
+
+
+def configure_link(node, interface, tc_params):
+	"""
+	Configure interface on node
+	:param node: node to configure
+	:param interface: interface to configure
+	:param tc_params: tuple (bandwidth, burst, latency)
+	:return: None
+	"""
+	bandwidth, burst, latency = tc_params
+	cmd = f"docker exec {node} tc qdisc add dev {interface} " \
+	      f"root tbf rate {bandwidth}mbit burst {burst}kb latency {latency}ms"
+	cmd_value = os.system(cmd)
+	if cmd_value != 0:
+		clear_cmd = f"docker exec {node} tc qdisc del dev {interface} root"
+		os.system(clear_cmd)
 		os.system(cmd)
 
 
@@ -109,11 +130,11 @@ if __name__ == "__main__":
 		if node_name not in node_vs_ip:
 			node_vs_ip[node_name] = []
 		node_vs_ip[node_name].append(node_param[0])
-	# python3 setup.py add_link '"r1-s1": ("10.0.4.0/24", (("r1", "10.0.4.2", "eth2"), ("s1", "10.0.4.4", "eth1")), 10)'
+	# python3 setup.py add_link '"r1-s1":("10.0.4.0/24", (("r1", "10.0.4.2", "eth2"), ("s1", "10.0.4.4", "eth1")), (1, 0.1, 10))'
 	if len(sys.argv) == 3:
 		print(sys.argv[1])
 		print(sys.argv[2])
-		if(str(sys.argv[1])=="add_link"):
+		if (str(sys.argv[1]) == "add_link"):
 			link = sys.argv[2].split(":")
 			link_name = ast.literal_eval(link[0])
 			link_param_str = link[1]
@@ -121,8 +142,9 @@ if __name__ == "__main__":
 			links[link_name] = link_param
 			create_subnet(link_param[0], link_name)
 			endpoints = link_param[1]
-			attach(endpoints[0][1], link_name, endpoints[0][0], endpoints[0][2])
-			attach(endpoints[1][1], link_name, endpoints[1][0], endpoints[1][2])
+			tc_params = link_param[2]
+			attach(endpoints[0][1], link_name, endpoints[0][0], endpoints[0][2], tc_params)
+			attach(endpoints[1][1], link_name, endpoints[1][0], endpoints[1][2], tc_params)
 		else:
 			print("Invalid Argument")
 	else:
@@ -142,9 +164,10 @@ if __name__ == "__main__":
 		# attach containers to networks
 		for link_name, link_param in links.items():
 			endpoints = link_param[1]
+			tc_params = link_param[2]
 			try:
-				attach(endpoints[0][1], link_name, endpoints[0][0], endpoints[0][2])
-				attach(endpoints[1][1], link_name, endpoints[1][0], endpoints[1][2])
+				attach(endpoints[0][1], link_name, endpoints[0][0], endpoints[0][2], tc_params)
+				attach(endpoints[1][1], link_name, endpoints[1][0], endpoints[1][2], tc_params)
 			except Exception as e:
 				print(e)
 
@@ -155,7 +178,7 @@ if __name__ == "__main__":
 	# Create Graph
 	for link_name, link_param in links.items():
 		endpoints = link_param[1]
-		band_width = link_param[2]
+		band_width = link_param[2][0]
 
 		if endpoints[0][0] not in node_vs_ip:
 			node_vs_ip[endpoints[0][0]] = []
@@ -168,7 +191,8 @@ if __name__ == "__main__":
 			graph[endpoints[0][0]] = []
 			connections[endpoints[0][0]] = {}
 		graph[endpoints[0][0]].append((endpoints[1][0], float(band_width)))
-		connections[endpoints[0][0]][endpoints[1][0]] = (endpoints[1][1], endpoints[0][2]) #ip of other node,eth of itself
+		connections[endpoints[0][0]][endpoints[1][0]] = (
+		endpoints[1][1], endpoints[0][2])  # ip of other node,eth of itself
 		if endpoints[1][0] not in graph:
 			graph[endpoints[1][0]] = []
 			connections[endpoints[1][0]] = {}
@@ -183,12 +207,12 @@ if __name__ == "__main__":
 				continue
 			prev_node = dist[node][1]
 			next_hop = node
-			while prev_node != start_node and dist[prev_node][0]!= float('inf'):
+			while prev_node != start_node and dist[prev_node][0] != float('inf'):
 				next_hop = prev_node
 				prev_node = dist[prev_node][1]
 			if dist[prev_node][0] == float('inf'):
 				continue
-			hops.append((node,next_hop))
+			hops.append((node, next_hop))
 		print(f"\nStart Node = {start_node}")
 		for dest_node, next_hop_node in hops:
 			next_hop_node_ip = connections[start_node][next_hop_node][0]
@@ -197,3 +221,10 @@ if __name__ == "__main__":
 				add_route(start_node, dest_node_ip, next_hop_node_ip, interface)
 			print(f"Destination Node = {dest_node}, Next hop = {next_hop_node}")
 
+# Configure bandwidth
+for link_name, link_param in links.items():
+	endpoint0 = link_param[1][0]
+	endpoint1 = link_param[1][1]
+	tc_params = link_param[2]
+	configure_link(endpoint0[0], endpoint0[2], tc_params)
+	configure_link(endpoint1[0], endpoint1[2], tc_params)
