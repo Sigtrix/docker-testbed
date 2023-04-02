ad7556ec13b0effe1c3d07e546158aa751ca5d68
Sigtrix <hes86@hi.is>
2023-04-01 20:07:04 -0400

    Adds results

--- bw-latency-experiment.py
+++ bw-latency-experiment.py
@@ -6,22 +6,23 @@ on an existing topology.
 """
 import subprocess
 import os
-import numpy as np
 import re
-from config import links
+import numpy as np
 from setup import configure_link, read_state_json
 import matplotlib.pyplot as plt
 
 
-server = {'name': 's1', 'ip': '10.0.3.4'}
+server = 'r1'
 client = 'c1'
 
-bandwidth_values = [1, 2, 4, 8, 16, 32, 64, 128]
+bandwidth_values = [1] + list(np.arange(10, 101, 10)) + list(np.arange(0, 10001, 250)[1:])
 bandwidth_results = []
+latency_const = 1
+burst_const = 12500
 
-num_links = 6  # number of links on path to and from client and server
 latency_values = list(np.arange(1, 20, 1)) + list(np.arange(20, 110, 10))
 latency_results = []
+bandwidth_const = 2
 
 
 def configure_params(links, params):
@@ -38,18 +39,18 @@ def configure_params(links, params):
 		configure_link(endpoint1[0], endpoint1[2], params)
 
 
-# setup iperf server on s1
-os.system(f"docker exec {server['name']} iperf -s &")
+# setup iperf server on r1
+os.system(f"docker exec {server} iperf -s &")
 
 for i in range(len(bandwidth_values)):
 	# update bw on all links
-	tc_params = (bandwidth_values[i], 2, 2)
+	tc_params = (bandwidth_values[i], burst_const, latency_const)
 	print(tc_params)
 	current_state = read_state_json()
 	links = current_state["links"]
 	configure_params(links, tc_params)
 
-	result = subprocess.run(['docker', 'exec', client, 'iperf', '-c', server['ip']], stdout=subprocess.PIPE)
+	result = subprocess.run(['docker', 'exec', client, 'iperf', '-c', server], stdout=subprocess.PIPE)
 	output = result.stdout.decode('utf-8')
 	print(output)
 
@@ -58,43 +59,46 @@ for i in range(len(bandwidth_values)):
 	bandwidth = float(output.split(' ')[-2])
 	if 'Kbits' in unit:
 		bandwidth /= 1000
+	elif 'Gbits' in unit:
+		bandwidth *= 1000
 	bandwidth_results.append(bandwidth)
 
 
-for i in range(len(latency_values)):
-	# update latency on all links
-	tc_params = (2, 2, latency_values[i])
-	print(tc_params)
-	current_state = read_state_json()
-	links = current_state["links"]
-	configure_params(links, tc_params)
-
-	result = subprocess.run(['docker', 'exec', client, 'ping', '-c', '10', server['ip']], stdout=subprocess.PIPE)
-	output = result.stdout.decode('utf-8')
-	print(output)
-
-	# parse output to get bandwidth
-	output = re.split('[ /]', output)
-	avg_latency = float(output[-4])
-	avg_link_latency = avg_latency/num_links
-	latency_results.append(avg_link_latency)
-
-
 # plot bandwidth test results
-plt.plot(bandwidth_values, bandwidth_results)
+plt.scatter(bandwidth_values, bandwidth_results, c='orange')
+plt.axline((0, 0), slope=1, c='black', linestyle='--')
 plt.xlabel('Bandwidth values configured with tc')
 plt.ylabel('Measured bandwidth using iperf')
-plt.title('Bandwidth comparison')
+plt.title(f'Bandwidth [Mbits/sec] comparison (latency {latency_const}ms)')
 plt.savefig('bandwidth-measurements')
 plt.show()
 
-# plot latency results
-plt.plot(latency_values, latency_results)
-plt.xlabel('Latency values configured with tc')
-plt.ylabel('Measured latency using ping')
-plt.title('Latency comparison')
-plt.savefig('latency-measurements')
-plt.show()
+
+# for i in range(len(latency_values)):
+# 	# update latency on all links
+# 	tc_params = (bandwidth_const, burst_const, latency_values[i])
+# 	print(tc_params)
+# 	current_state = read_state_json()
+# 	links = current_state["links"]
+# 	configure_params(links, tc_params)
+#
+# 	result = subprocess.run(['docker', 'exec', client, 'ping', '-c', '10', server], stdout=subprocess.PIPE)
+# 	output = result.stdout.decode('utf-8')
+# 	print(output)
+#
+# 	# parse output to get bandwidth
+# 	output = re.split('[ /]', output)
+# 	avg_latency = float(output[-4])/2
+# 	latency_results.append(avg_latency)
+#
+# # plot latency results
+# plt.scatter(latency_values, latency_results, c='orange')
+# plt.axline((0, 0), slope=1, c='black', linestyle='--')
+# plt.xlabel('Latency values configured with tc')
+# plt.ylabel('Measured latency using ping')
+# plt.title('Latency comparison [ms]')
+# plt.savefig('latency-measurements')
+# plt.show()
 
 
 
