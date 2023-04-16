ddf35b7c3b03e19d782dad6e1418420dacc8501b
Sigtrix <hes86@hi.is>
2023-04-16 10:36:16 -0400

    ...

--- real-world-experiment-load-determined.py
+++ real-world-experiment-load-determined.py
@@ -15,14 +15,14 @@ server = {'name': 'ue1', 'ip': '10.0.3.4'}
 contesting_client = 'r1'
 client = 'ue4'
 
-n_iter = 10
-n_streams = 5
+n_iter = 5
+n_streams = 3
 latency_const = 1
 burst_const = 12500
 
 # setup iperf server on server and bottleneck dest
 os.system(f"docker exec {server['name']} iperf -s &")
-os.system(f"docker exec {bottleneck_link_dest['name']} iperf -s &")
+os.system(f"docker exec {bottleneck_link_dest['name']} iperf -u -s &")
 
 # generate background traffic on path from client to server
 os.system(f"docker exec {client} iperf -t 0 -c {server['ip']} &")
@@ -31,7 +31,7 @@ os.system(f"docker exec {client} iperf -t 0 -c {server['ip']} &")
 bandwidth_est = []
 for i in range(n_streams):
 	# generate background traffic on bottleneck link
-	os.system(f"docker exec {contesting_client} iperf -t 0 -c {bottleneck_link_dest['ip']} &")
+	os.system(f"docker exec {contesting_client} iperf -t 0 -u -b 10M -c {bottleneck_link_dest['ip']} &")
 	bottleneck_bandwidth = []
 	for j in range(n_iter):
 		result = subprocess.run(['docker', 'exec', client, './pathneck-1.3/pathneck', '-o', server['ip']], stdout=subprocess.PIPE)
