280a4ed617d0e74cd7434a461d4a7670b2bc5aa3
sroyc123 <sr30@iitbbs.ac.in>
2023-05-07 05:17:16 +0000

    Better results

--- src/experiment.py
+++ src/experiment.py
@@ -9,20 +9,20 @@ import seaborn as sns
 
 n_iter = 20
 bottleneck_bandwidth = []
-data = {'00': [], '01': [], '02': [], '03': []}
+data = {'00': [], '01': [], '02': [], '03': [], '04': [], '05': []}
 
 bottleneck_link_dest = {'name': 'r3', 'ip': '10.0.3.3'}
-server = {'name': 's1', 'ip': '10.0.5.5'}
+server = {'name': 's1', 'ip': '10.0.4.4'}
 contesting_client = 'c2'
 client = 'c1'
 
 # setup iperf server on bottleneck link destination
 os.system(f"docker exec {server['name']} iperf -s &")
-os.system(f"docker exec {bottleneck_link_dest['name']} iperf -s &")
+# os.system(f"docker exec {bottleneck_link_dest['name']} iperf -s &")
 
 # generate background traffic from c2 to r3 (through r2)
 os.system(f"docker exec {client} iperf -t 0 -c {server['ip']} &")
-os.system(f"docker exec {contesting_client} iperf -t 0 -c {bottleneck_link_dest['ip']} &")
+# os.system(f"docker exec {contesting_client} iperf -t 0 -c {bottleneck_link_dest['ip']} &")
 
 
 for i in range(n_iter):
